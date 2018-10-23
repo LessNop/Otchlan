ac1eb8da815262938cc285b0df9cb8dde39d3cf5
LessNop <43969986+LessNop@users.noreply.github.com>
2018-10-23 19:38:29 +0200

    Add files via upload

---
+++ src/op/lessnop/MainData.java
@@ -0,0 +1,9 @@
+package op.lessnop;
+
+public class MainData extends Zapis{
+
+	public MainData(Main main) {
+		super(main, "zapisItemow.yml");
+	}
+	
+}
---
+++ src/op/lessnop/Vars.java
@@ -0,0 +1,15 @@
+package op.lessnop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class Vars {
+	
+	public static List<Player> list = new ArrayList<Player>();
+	public static List<ItemStack> itemy = new ArrayList<ItemStack>();
+	public static boolean b;
+	
+}
